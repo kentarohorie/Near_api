0c546b1f2f3b6615c5cb04e937e5c7a407e4e5ff
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-06-14 18:23:33 +0900

    api user.create の実装

--- app/apis/api/ver1/users.rb
+++ app/apis/api/ver1/users.rb
@@ -3,9 +3,21 @@ module API
     class Users < Grape::API
       format :json
       formatter :json, Grape::Formatter::Jbuilder
-      #/api/v1/users
+
+      helpers do
+        def create_params
+          ActionController::Parameters.new(params).permit(:facebook_id, :age, :gender, :name)
+        end
+      end
+
+      desc 'GET /api/v1/users'
       get '/users', jbuilder: 'api/ver1/users/index' do
         @users = User.all
+      end
+
+      desc 'POST /api/v1/users/create'
+      post '/users/create', jbuilder: 'api/ver1/users/create' do
+        @user = User.create(create_params)
       end
 
     end
---
+++ app/views/apis/api/ver1/users/create.jbuilder
@@ -0,0 +1,4 @@
+json.fbID @user.facebook_id
+json.age @user.age
+json.gender @user.gender
+json.name @user.name
