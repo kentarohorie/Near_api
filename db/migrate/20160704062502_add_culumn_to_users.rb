3b4b943f5c1843816058ff0333d23a097a7af859
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-07-05 19:53:27 +0900

    change users table column

---
+++ db/migrate/20160704062502_add_culumn_to_users.rb
@@ -0,0 +1,7 @@
+class AddCulumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :greeting, :text
+    add_column :users, :work, :string, default: ""
+    add_column :users, :school, :string, default: ""
+  end
+end
