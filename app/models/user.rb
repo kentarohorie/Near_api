cd95c70bb27b7ed232dc0d4012de8ac293efc131
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-07-11 03:50:27 +0900

    refectoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :messages
-  has_many :user_rooms
-  has_many :rooms, through: :user_rooms
+  # has_many :user_rooms
+  # has_many :rooms, through: :user_rooms
 
   require 'json'
   require 'open-uri'
